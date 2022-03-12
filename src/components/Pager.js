e247b0091c789087400c02dadb1d174377f8fe06
ketyykes <xi4869@gmail.com>
2022-03-12 21:56:12 +0800

    finish pagerJS component

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -1,41 +1,38 @@
-// import React, { useState } from "react";
-// import * as styles from "../styles/tech-page.module.scss";
+import React, { useState } from "react";
+import * as styles from "../styles/tech-page.module.scss";
 
-// const Pager = ({ allItem ,setAllItem}) => {
-//   let perPage = 4;
-//   console.log(allItem);
+const Pager = ({ allItem, setAllItem, allmarkdownArticle, perPage }) => {
+  let totalPages = Math.ceil(allmarkdownArticle.length / perPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { pageButton, pagination } = styles;
 
-//   let totalPages = Math.ceil(allItem.length / perPage);
-//   const [currentPage, setCurrentPage] = useState(1);
-// //   const [markdownArticle, setMarkdownArticle] = useState(
-// //     allItem.slice((currentPage - 1) * perPage, perPage * currentPage)
-// //   );
-//   const { content, card, pageButton, pagination } = styles;
+  const pageHandler = (e) => {
+    let targetValue = e.target.value;
+    setCurrentPage(targetValue);
+    setAllItem(
+      allmarkdownArticle.slice(
+        (targetValue - 1) * perPage,
+        perPage * targetValue
+      )
+    );
+  };
 
-//   const pageHandler = (e) => {
-//     let targetValue = e.target.value;
-//     setCurrentPage(targetValue);
-//     setAllItem(
-//       allItem.slice((targetValue - 1) * perPage, perPage * targetValue)
-//     );
-//   };
+  return (
+    <ul className={pagination}>
+      {Array.from({ length: totalPages }, (el, index) => (
+        <li
+          className={pageButton}
+          key={index}
+          value={index + 1}
+          onClick={pageHandler}
+          role="button"
+          tabIndex={index + 1}
+        >
+          {index + 1}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-//   return (
-//     <ul className={pagination}>
-//       {Array.from({ length: totalPages }, (el, index) => (
-//         <li
-//           className={pageButton}
-//           key={index}
-//           value={index + 1}
-//           onClick={pageHandler}
-//           role="button"
-//           tabIndex={index + 1}
-//         >
-//           {index + 1}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default Pager;
+export default Pager;
--- src/pages/tech-page.js
+++ src/pages/tech-page.js
@@ -7,35 +7,17 @@ import * as styles from "../styles/tech-page.module.scss";
 const dayjs = require("dayjs");
 
 const TechPage = ({ data }) => {
-  const { content, card, pageButton, pagination } = styles;
+  const { content, card } = styles;
   let perPage = 4;
-  // const [allItem ,setAllItem] = useState(data.allMarkdownRemark.nodes);
-  // const allItem =  data.allMarkdownRemark.nodes;
-
-  
-
   const allmarkdownArticle = data.allMarkdownRemark.nodes;
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [markdownArticle, setMarkdownArticle] = useState(
-    allmarkdownArticle.slice((currentPage - 1) * perPage, perPage * currentPage)
+  const [allItem, setAllItem] = useState(
+    allmarkdownArticle.slice(0 , perPage * 1)
   );
-  let totalPages = Math.ceil(allmarkdownArticle.length / perPage);
-  const pageHandler = (e) => {
-    let targetValue = e.target.value;
-    setCurrentPage(targetValue);
-    setMarkdownArticle(
-      data.allMarkdownRemark.nodes.slice(
-        (targetValue - 1) * perPage,
-        perPage * targetValue
-      )
-    );
-  };
   return (
     <>
       <Layout>
         <div className={content}>
-          {markdownArticle.map((article, index) => (
+          {allItem.map((article, index) => (
             <div className={card} key={article.id}>
               <h3>
                 <Link
@@ -50,24 +32,14 @@ const TechPage = ({ data }) => {
               <p>{dayjs(article.frontmatter.date).format("YYYY-MM-DD ddd")}</p>
             </div>
           ))}
-          <ul className={pagination}>
-            {Array.from({ length: totalPages }, (el, index) => (
-              <li
-                className={pageButton}
-                key={index}
-                value={index + 1}
-                onClick={pageHandler}
-                role = "button"  tabIndex={index + 1}
-              >
-                {index + 1}
-              </li>
-            ))}
-          </ul>
-              {/* <Pager allItem={allItem} setAllItem={setAllItem} /> */}
+          <Pager
+            allItem={allItem}
+            setAllItem={setAllItem}
+            allmarkdownArticle={allmarkdownArticle}
+            perPage={perPage}
 
+          />
         </div>
-
-    
       </Layout>
     </>
   );
