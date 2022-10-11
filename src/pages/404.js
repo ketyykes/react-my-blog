ef0d9933cfd3e280291860c530f43c9d6660dbf0
ketyykes <xi4869@gmail.com>
2022-10-12 02:52:01 +0800

    delete head in 404 not found.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../components/header/Header";
-import Head from "../components/Head/Head";
 import Footer from "../components/footer/Footer";
 import Seo from "../components/seo/Seo";
 import { not_found } from "../styles/pages-styles/notfound.module.scss";
