4f388229c72fc31d9aa58e1abf2e644afc0de406
danny <xi4869@gmail.com>
2024-03-25 14:51:44 +0800

    feat: Add gatsby-ssr.jsx file with wrapPageElement function

---
+++ gatsby-ssr.jsx
@@ -0,0 +1,12 @@
+const React = require("react");
+require("./src/styles/global.scss");
+const Layout = require("./src/components/layout/Layout").default;
+exports.wrapPageElement = ({ element, props }) => {
+  const pathsWithBanner = ["/photo/", "/", "/tags/"];
+  const banner = pathsWithBanner.includes(props.location.pathname);
+  return (
+    <Layout banner={banner} {...props}>
+      {element}
+    </Layout>
+  );
+};
