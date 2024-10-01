ba78914925de0e4223bf2d5420526f6ff83d55a7
danny <xi4869@gmail.com>
2024-10-02 03:20:39 +0800

    feat: Add ArticleCard component
    This commit adds a new component called ArticleCard, which is responsible for rendering individual article cards in the application. The ArticleCard component imports React, Link from Gatsby, and dayjs for date formatting. It receives props such as title, date, and linkPath to display the article information. The component is styled using the card class from the articleList.module.scss file.
    
    Co-authored-by: [Co-author Name]

---
+++ src/components/articleCard/ArticleCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+import { card } from "./articleList.module.scss";
+import dayjs from "dayjs";
+
+const ArticleCard = ({ title, date, linkPath }) => (
+	<div className={card}>
+		<h3>
+			<Link to={linkPath}>{title}</Link>
+		</h3>
+		<p>{dayjs(date).format("YYYY-MM-DD ddd")}</p>
+	</div>
+);
+
+export default ArticleCard;
