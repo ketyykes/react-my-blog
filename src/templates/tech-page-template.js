9f309c19de0e78068ea5b61d0deb06a797b34074
danny <xi4869@gmail.com>
2024-10-02 00:41:29 +0800

    refactor: 修改 TechAndTagsPage 的輸入框寬度和分頁導航路徑處理函式
    調整 TechAndTagsPage 的輸入框寬度，並修改 Pager.js 中的 handleChange 函式以處理分頁導航路徑。

--- src/templates/tech-page-template.js
+++ src/templates/tech-page-template.js
@@ -1,39 +1,55 @@
-import React, { useState } from "react";
-import { Pager, Seo } from "../components";
-import { graphql, Link } from "gatsby";
+import React, { useState, useEffect } from "react";
+import { Pager, Seo, ArticleList } from "../components";
+import { graphql, Link, navigate } from "gatsby";
+import TextField from "@mui/material/TextField";
+
+import {
+	filterArticles,
+	getPaginatedArticles,
+	updateURLQuery,
+	navigateToFirstPage,
+} from "./module/module.js";
 import {
 	content,
-	card,
+	input,
 } from "../styles/templates-styles/tech-and-tags-page.module.scss";
 
-const dayjs = require("dayjs");
 const TechPageTemplate = ({ pageContext, data }) => {
-	const allmarkdownArticle = data.allMarkdownRemark.nodes;
+	const allMarkdownArticles = data.allMarkdownRemark.nodes;
 	const { perPage, currentPage } = pageContext;
-	const [allItem, setAllItem] = useState(
-		allmarkdownArticle.slice(perPage * (currentPage - 1), perPage * currentPage)
+
+	// 初始從 URL 讀取搜尋參數
+	const initialSearchValue =
+		new URLSearchParams(window.location.search).get("search") || "";
+
+	// 使用 useState 來保存 TextField 的輸入值
+	const [inputValue, setInputValue] = useState(initialSearchValue);
+
+	// 篩選文章標題
+	const filteredArticles = filterArticles(allMarkdownArticles, inputValue);
+
+	// 設置分頁後顯示的資料
+	const paginatedArticles = getPaginatedArticles(
+		filteredArticles,
+		perPage,
+		currentPage
 	);
+
+	// 當輸入值改變時，更新 URL 的 query string
+	useEffect(() => {
+		updateURLQuery(inputValue);
+		if (currentPage > Math.ceil(filteredArticles.length / perPage)) {
+			navigateToFirstPage(inputValue);
+		}
+	}, [inputValue]);
+
 	return (
 		<div className={content}>
-			{allItem.map((article) => (
-				<div className={card} key={article.id}>
-					<h3>
-						<Link
-							to={
-								"/tech-page/" +
-								dayjs(article.frontmatter.date).format("YYYY-MM-DD ddd")
-							}
-						>
-							{article.frontmatter.title}
-						</Link>
-					</h3>
-					<p>{dayjs(article.frontmatter.date).format("YYYY-MM-DD ddd")}</p>
-				</div>
-			))}
-			<Pager
-				allItem={allItem}
-				setAllItem={setAllItem}
-				allmarkdownArticle={allmarkdownArticle}
+			<SearchInput inputValue={inputValue} setInputValue={setInputValue} />
+			<ArticleList articles={paginatedArticles} />
+			<PaginationOrMessage
+				articles={paginatedArticles}
+				filteredArticles={filteredArticles}
 				perPage={perPage}
 				currentPage={currentPage}
 			/>
@@ -41,13 +57,44 @@ const TechPageTemplate = ({ pageContext, data }) => {
 	);
 };
 
+// 搜尋輸入元件
+const SearchInput = ({ inputValue, setInputValue }) => (
+	<TextField
+		label="搜尋文章標題"
+		variant="outlined"
+		margin="normal"
+		value={inputValue}
+		className={input}
+		onChange={(e) => setInputValue(e.target.value)}
+	/>
+);
+
+// 分頁器或無文章訊息元件
+const PaginationOrMessage = ({
+	articles,
+	filteredArticles,
+	perPage,
+	currentPage,
+}) =>
+	articles.length === 0 ? (
+		<p>找不到符合條件的文章</p>
+	) : (
+		<Pager
+			allmarkdownArticle={filteredArticles}
+			perPage={perPage}
+			currentPage={currentPage}
+		/>
+	);
+
 export default TechPageTemplate;
+
 export const Head = () => (
 	<>
 		<html lang="zh-Hant-TW" />
-		<Seo title="Tech-page" />;
+		<Seo title="Tech-page" />
 	</>
 );
+
 export const query = graphql`
 	{
 		allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
